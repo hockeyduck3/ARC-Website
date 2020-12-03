0c9aebe0197fc9cbc4d2c169d5ec1e42da1ef2a9
LJ Spencer <hockeyduck3@gmail.com>
2020-12-03 09:32:17 -1000

    Added main.js file

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// This solution was found on https://stackoverflow.com/questions/46736823/how-can-i-close-my-bootstrap-4-navbar-collapse-menu-when-clicking-outside-the-me by Priyansh Garg
+// This function will make it so that the navbar will close by itself on mobile, if the user clicks anywhere else on the page.
+$(document).click((event) => {
+    let click = $(event.target);
+
+    if ($('.navbar-toggler').attr('aria-expanded') === 'true' && click.closest('.navbar').length === 0) {
+        $('button[aria-expanded="true"]').click();
+    }
+});
