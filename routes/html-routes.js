049cfab57409d9cbc610623e6c675586aa0aa6bb
LJ Spencer <hockeyduck3@gmail.com>
2020-12-03 21:47:30 -1000

    Added the rest of the routes, and added some if/else statements to the main handlebars file.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+// Each route has a true boolean attached to it. This is for the navbar so the webpage knows which page is "active".
 router.get('/', (req, page) => {
-    page.render('index');
+    page.render('index', {
+        home: true
+    });
+});
+
+router.get('/about', (req, page) => {
+    page.render('about', {
+        about: true
+    });
+});
+
+router.get('/programs', (req, page) => {
+    page.render('programs', {
+        programs: true
+    });
+});
+
+router.get('/gallery', (req, page) => {
+    page.render('gallery', {
+        gallery: true
+    });
+});
+
+router.get('/involved', (req, page) => {
+    page.render('involved', {
+        involved: true
+    });
 });
 
 module.exports = router;
