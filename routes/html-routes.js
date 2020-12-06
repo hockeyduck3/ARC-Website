13f625ef4991c1878ab31cc4857182e2253a8869
LJ Spencer <hockeyduck3@gmail.com>
2020-12-05 22:56:54 -1000

    Added a style key/value to each route, also added some more styling.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,33 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 // Each route has a true boolean attached to it. This is for the navbar so the webpage knows which page is "active".
+// Each route also has a "style" attached. This will make it so each page can have it's own css file without needing to load in the others.
 router.get('/', (req, page) => {
     page.render('index', {
-        home: true
+        home: true,
+        style: 'index'
     });
 });
 
 router.get('/about', (req, page) => {
     page.render('about', {
-        about: true
+        about: true,
+        style: 'about'
     });
 });
 
 router.get('/programs', (req, page) => {
     page.render('programs', {
-        programs: true
+        programs: true,
+        style: 'programs'
     });
 });
 
 router.get('/gallery', (req, page) => {
     page.render('gallery', {
-        gallery: true
+        gallery: true,
+        style: 'gallery'
     });
 });
 
 router.get('/involved', (req, page) => {
     page.render('involved', {
-        involved: true
+        involved: true,
+        style: 'involved'
     });
 });
 
